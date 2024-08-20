62fc9451b07a187769a11203e8ede9524adf0073
ERNI ROHMAWATI <89213152+ernirohmawati@users.noreply.github.com>
2024-08-20 21:47:41 +0700

    Add files via upload

---
+++ tes-demo.js
@@ -0,0 +1,10 @@
+describe ('Test Suit Name', () =>{
+    it('Test Case 1', () =>{
+        //implementasi test code here
+    });
+    it('Test Case 2', () =>{
+        //implementasi test code here
+    });
+})
+
+
